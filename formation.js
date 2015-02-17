a5e2c923d761febcd3360c415d6819962d0f99af
mickey <mikio.nozaki@gmail.com>
2015-02-18 01:33:48 +0900

    フォーメーション切り替えを追加

--- formation.js
+++ formation.js
@@ -9,46 +9,137 @@ if(typeof Football!=='object'){var Football={};}
         this.width = this.r*2;
         this.height = this.r*2;
         
+        // player color
+        this.teamColorA = '#DF013A';
+        this.teamColorB = '#2E2EFE';
+
+        // team name
+        this.selectA = 'teamA';
+        this.selectB = 'teamB';
+
+        // team canvas
+        this.canvasIdA = 'canvasA_';
+        this.canvasIdB = 'canvasB_';
+        
         // start
         this._init();
     };
+    
     /**
      * *
      * @private
      */
     Formation.prototype._init=function() {
-        var c, self = this;
-
-
-        // top, left
-        this.addPlayer(300,25);
+        var self = this,
+            pathA,pathB;
         
-        this.addPlayer(100,200);
-        this.addPlayer(250,150);
-        this.addPlayer(350,150);
-        this.addPlayer(500,200);
-
-        this.addPlayer(200,350);
-        this.addPlayer(300,300);
-        this.addPlayer(400,350);
+        // 初期設定
+        pathA = self.getFormationFile($('[name='+self.selectA+']').val(), "teamA");
+        pathB = self.getFormationFile($('[name='+self.selectB+']').val(), "teamB");
         
-        //this.addRect(50,150);
-        //this.addRect(450,150);
-        //this.addRect(450,150);
+        self.addFormation(pathA, self.teamColorA, self.canvasIdA);
+        self.addFormation(pathB, self.teamColorB, self.canvasIdB);
 
-        //this.addRect(50,50);
+        self._bind();
+    };
 
-        // 移動
-        //$('#canvas1').css({top:300,left:50});
-        //
-        //$('#canvas2').css({top:100,left:100});
-        //$('#canvas3').css({top:200,left:100});
-        //$('#canvas4').css({top:300,left:100});
-        //$('#canvas5').css({top:400,left:100});
+    /**
+     * *
+     * @private
+     */
+    Formation.prototype._bind=function() {
+        var self = this;
+
+        $('[name='+self.selectA+']').change(function() {
+            pathA = self.getFormationFile($(this).val(), "teamA");
+            self.moveFormation(pathA, self.canvasIdA);
+        });
+
+        $('[name='+self.selectB+']').change(function() {
+            pathB = self.getFormationFile($(this).val(), "teamB");
+            self.moveFormation(pathB, self.canvasIdB);
+        });
+
+    };
+
+    /**
+     * *
+     * @param formation
+     * @param color
+     */
+    Formation.prototype.addFormation=function(formation, color, teamId) {
+        var self = this;
+        $.getJSON(formation , function(data) {
+            for(var i = 0; i < data.length; i++) {
+                self.addPlayer(data[i].x,data[i].y, color, teamId);
+            }
+        });
         
     };
 
+    /**
+     * *
+     * @param formation
+     * @param color
+     */
+    Formation.prototype.moveFormation=function(formation, canvasId) {
+        var self = this;
+        $.getJSON(formation , function(data) {
+            for(var i = 0; i < data.length; i++) {
+                $('#'+ canvasId + data[i].id).css({top:data[i].x,left:data[i].y});
+            }
+        });
 
+    };
+
+    /**
+     * *
+     * @param formation
+     * @param team
+     * @returns {string}
+     */
+    Formation.prototype.getFormationFile=function(formation, team) {
+        return './data/'+team+'/'+formation+'.json';
+        
+    };
+    
+
+    /**
+     * *
+     * @param x
+     * @param y
+     * @returns {boolean}
+     */
+    Formation.prototype.addPlayer = function(x, y, color, teamId) {
+        var self = this, id, canvas, context;
+        
+        id = self.getId(teamId);
+
+        // player canvas
+        canvas = self.addCanvas(self.width, self.height, id);
+        if (!canvas) return false;
+
+        // create player
+        context = canvas.getContext('2d');
+        context.beginPath();
+        //context.shadowBlur = 10; //ぼかし
+        //context.shadowColor = "green"; //ぼかし
+
+        context.arc(20, 20, self.r, 0, Math.PI*2, false); // x,y, 半径,
+        context.fillStyle = color;
+        context.fill();
+        context.lineWidth = 0.5;
+        context.stroke();
+        context.strokeStyle = 'black';
+        //context.translate(100, 50);
+        context.closePath();
+        
+        // dragable
+        $(canvas).draggable({ containment: '#pitch_area', scroll: false });
+        // move
+        $('#'+id).css({top:x,left:y});
+        
+    };
     /**
      * *
      * @param width
@@ -58,8 +149,7 @@ if(typeof Football!=='object'){var Football={};}
      * @param y
      * @returns {*}
      */
-    Formation.prototype.addCanvas = function(width, height, id, x,y) {
-        //Formation.prototype.addCanvas = function(width, height, id) {
+    Formation.prototype.addCanvas = function(width, height, id) {
         // Canvas要素を追加
         $('#pitch_area').append(
             $('<canvas></canvas>')
@@ -79,51 +169,17 @@ if(typeof Football!=='object'){var Football={};}
      * canvasのidを返す *
      * @returns {string}
      */
-    Formation.prototype.getId = function() {
+    Formation.prototype.getId = function(teamId) {
         var self = this,
-            screen,id;
+            screen,id,canvasLength;
         screen = document.getElementById('pitch_area');
-        id = 'canvas' + (screen.getElementsByTagName('canvas').length + 1).toString();
+        canvasLength = $("[id^="+teamId+"]").length;
+        id = teamId + (canvasLength + 1).toString();
         return id;
     };
 
-    /**
-     * *
-     * @param x
-     * @param y
-     * @returns {boolean}
-     */
-    Formation.prototype.addPlayer = function(x, y) {
-        var self = this, id, canvas, context;
-        
-        id = this.getId();
-
-        // player canvas
-        canvas = this.addCanvas(self.width, self.height, id, x,y);
-        if (!canvas) return false;
-
-        context = canvas.getContext('2d');
-        // create player
-        context.beginPath();
-        //context.shadowBlur = 10; //ぼかし
-        //context.shadowColor = "green"; //ぼかし
-
-
-        context.arc(20, 20, self.r, 0, Math.PI*2, false); // x,y, 半径,
-        context.fillStyle ='rgb(192, 80, 77)'; // 赤色
-        context.fill();
-        context.lineWidth = 0.5;
-        context.stroke();
-        context.strokeStyle = 'black';
-        //context.translate(100, 50);
-        context.closePath();
-        
-        // dragable
-        $(canvas).draggable({ containment: '#pitch_area', scroll: false });
-        // move
-        $('#'+id).css({top:x,left:y});
-        
-    };
-
+    
+    
+    
 }(jQuery));
 $(function(){new Football.Formation();});
